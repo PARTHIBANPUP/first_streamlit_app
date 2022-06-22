303718c8edf5585011341b2b0ce66e8094208a63
PARTHIBANPUP <108000690+PARTHIBANPUP@users.noreply.github.com>
2022-06-22 20:50:06 +0530

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy diner')
-streamlit.header('BREAKFAST MENU')
-streamlit.text('omega 3 and oatmeal')
-streamlit.text('kale,spinach and smoothie')
+streamlit.header('BREAKFAST favourites')
+streamlit.text(':fish:omega 3 and oatmeal')
+streamlit.text(':beers:kale,spinach and smoothie')
+streamlit.text(':avacado:avacado')
