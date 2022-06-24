69f069f1c934bee82ed63ceda71e8ce0b30e8ce9
PARTHIBANPUP <108000690+PARTHIBANPUP@users.noreply.github.com>
2022-06-25 01:26:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ streamlit.text('🥗 kale,spinach and smoothie')
 streamlit.text('🥑 avacado')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #import pandas
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -29,10 +28,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 except urlerror as e:
   STREAMLIT.ERROR()
-
 streamlit.stop()
 #import snowflake.connector
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
