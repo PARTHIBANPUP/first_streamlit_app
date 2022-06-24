1297921f15fd776d32f48e5e3dde921188edd0d0
PARTHIBANPUP <108000690+PARTHIBANPUP@users.noreply.github.com>
2022-06-25 01:48:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,12 @@ streamlit.text('🥣 omega 3 and oatmeal')
 streamlit.text('🥗 kale,spinach and smoothie')
 streamlit.text('🥑 avacado')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -30,13 +25,11 @@ except URLError as e:
   STREAMLIT.ERROR()
 streamlit.stop()
 #import snowflake.connector
-
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
 streamlit.write('Thanks for adding', add_my_fruit)
 DEF INSERT_ROW_SNOWFLAKE(new_fruit):
   with my_cnx.cursor()as my_cur:
